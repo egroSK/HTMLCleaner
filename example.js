f4879ace96aefea59ad68c53f5adfa8714fac9cc
Matej Paulech <matej.paulech@gmail.com>
2011-11-05 15:58:36 +0100

    update example

--- example.js
+++ example.js
@@ -1,37 +1,17 @@
-var fs = require('fs');
 var HTMLCleaner = require('./');
 
 var cleaner = new HTMLCleaner();
 
-/// Output to variable
-var out;
-cleaner.on('start', function () {
-	out = '';
-})
-cleaner.on('data', function (data) {
-	out += data;
-});
-cleaner.on('end', function () {
-	console.log('cleaned data: \n' + out);
-});
-cleaner.on('error', function (err) {
-	console.error(err);
+cleaner.cleanString('<p>test</p>', function (err, data) {
+	if (err) {
+		return console.error('HTMLCleaner error: ' + err);
+	}
+	console.log(data);
 });
 
-/// Output to file
-// var ws;
-// cleaner.on('start', function () {
-// 	ws = fs.createWriteStream('example_output.xml', {encoding: 'utf8'});
-// })
-// cleaner.on('data', function (data) {
-// 	ws.write(data);
+// cleaner.cleanFile('example_file.js', function (err, data) {
+// 	if (err) {
+// 		return console.error('HTMLCleaner error: ' + err);
+// 	}
+// 	console.log(data);
 // });
-// cleaner.on('end', function () {
-// 	console.log('cleaned data are in file: example_output.xml');
-// });
-// cleaner.on('error', function(err) {
-// 	console.error(err);
-// });
-
-cleaner.cleanString('<p>test</p>');
-// cleaner.cleanFile('example_file.js');
